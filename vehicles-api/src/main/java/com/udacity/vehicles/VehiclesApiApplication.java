d699bfb138571818bac736e68b2b54ac88d3a565
Rajashekar Chintalapati <rchintalapati@walmartlabs.com>
2019-09-20 10:02:35 -0700

    feat: added eureka discovery

--- vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
@@ -7,16 +7,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.reactive.function.client.WebClient;
 
 /**
- * Launches a Spring Boot application for the Vehicles API,
- * initializes the car manufacturers in the database,
- * and launches web clients to communicate with maps and pricing.
+ * Launches a Spring Boot application for the Vehicles API, initializes the car
+ * manufacturers in the database, and launches web clients to communicate with
+ * maps and pricing.
  */
 @SpringBootApplication
+@EnableEurekaClient
 @EnableJpaAuditing
 public class VehiclesApiApplication {
 
@@ -26,6 +28,7 @@ public class VehiclesApiApplication {
 
     /**
      * Initializes the car manufacturers available to the Vehicle API.
+     * 
      * @param repository where the manufacturer information persists.
      * @return the car manufacturers to add to the related repository
      */
@@ -47,20 +50,22 @@ public class VehiclesApiApplication {
 
     /**
      * Web Client for the maps (location) API
+     * 
      * @param endpoint where to communicate for the maps API
      * @return created maps endpoint
      */
-    @Bean(name="maps")
+    @Bean(name = "maps")
     public WebClient webClientMaps(@Value("${maps.endpoint}") String endpoint) {
         return WebClient.create(endpoint);
     }
 
     /**
      * Web Client for the pricing API
+     * 
      * @param endpoint where to communicate for the pricing API
      * @return created pricing endpoint
      */
-    @Bean(name="pricing")
+    @Bean(name = "pricing")
     public WebClient webClientPricing(@Value("${pricing.endpoint}") String endpoint) {
         return WebClient.create(endpoint);
     }
--- vehicles-api/src/main/java/com/udacity/vehicles/config/SecurityConfig.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.udacity.vehicles.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
